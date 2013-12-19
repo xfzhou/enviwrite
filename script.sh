b375de4e92d37f343da9774b3ca8e0ece22fcaa3
xfzhou <zhou09ustcer@gmail.com>
2013-12-19 10:08:39 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 iquery -aq "unload_library('enviwrite')"
-scidb.py stopall envi
+scidb.py stopall testDB
 sudo rm /opt/scidb/13.9/lib/scidb/plugins/libenviwrite.so
 make clean
 make
 sudo cp libenviwrite.so /opt/scidb/13.9/lib/scidb/plugins
-scidb.py startall envi
+scidb.py startall testDB
 iquery -aq "load_library('enviwrite')"
 
